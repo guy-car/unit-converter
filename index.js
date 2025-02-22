ab79cbce2aa744d2283988b5c1649022e60bba24
guy-car <guil.crn@gmail.com>
2025-02-22 18:43:01 -0500

    styled with gifs

--- index.js
+++ index.js
@@ -14,6 +14,39 @@ const massEl = document.getElementById("mass-el")
 const userInput = document.getElementById("user-input")
 const convBtn = document.getElementById("conv-btn")
 
+const gifEl = document.getElementById("gif-el")
+// gifEl.innerHTML = `<img src="gifs/Blow Your Mind Wow GIF by Product Hunt.gif">`
+
+const gifArr = ["gifs/Blow Your Mind Wow GIF by Product Hunt.gif",
+    "gifs/Canadian Drake GIF by MOODMAN.gif",
+    "gifs/Canadian Wow GIF by DJ Khaled.gif",
+    "gifs/Cat Wow GIF.gif", 
+    "gifs/Disbelief Reaction GIF.gif", 
+    "gifs/Disbelief Reaction GIF.gif", 
+    "gifs/Hotline Bling Pizza GIF.gif", 
+    "gifs/Keanu Reeves Reaction GIF.gif",
+    "gifs/Kentucky Derby Ok GIF.gif",
+    "gifs/nice GIF.gif",
+    "gifs/Oh My God Wow GIF by 9Now.gif",
+    "gifs/Oh My God Wow GIF by MOODMAN.gif",
+    "gifs/Oh My God Wow GIF by reactionseditor.gif",
+    "gifs/Proud Of You Yes GIF.gif",
+    "gifs/Reaction Yes GIF by Paper Triangles.gif",
+    "gifs/Sacha Baron Cohen Thumbs Up GIF by Amazon Prime Video.gif",
+    "gifs/Samantha Jones Wow GIF by Max.gif",
+    "gifs/Video Games Yes GIF by Call of Duty.gif",
+    "gifs/Well Done Reaction GIF.gif"
+]
+
+// function choseGif() {
+//     let randomGif = Math.floor(Math.random() * 7 )
+//     gifEl.innerHTML = `<img src="${gifArr[randomGif]}">`
+//     }
+//     choseGif()
+
+let randomGif = Math.floor(Math.random() * 7 )
+console.log(gifArr[randomGif])
+
 convBtn.addEventListener("click", function(){
     // User value
     let num = userInput.value
@@ -29,13 +62,7 @@ convBtn.addEventListener("click", function(){
     let kiloToPounds = num * massConv
     let poundsToKilo = num / massConv
     massEl.innerHTML = `${num} kilos = ${kiloToPounds.toFixed(3)} pounds | ${num} pounds = ${poundsToKilo.toFixed(3)} kilos`
-
-}
-
-)
-
-
-// function convert() {
-
-
-// }
+    // Random gif
+    let randomGif = Math.floor(Math.random() * gifArr.length )
+    gifEl.innerHTML = `<img src="${gifArr[randomGif]}">`
+})
